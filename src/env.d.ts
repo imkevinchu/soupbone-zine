edc29db03f9bad64a22f281253f636b0d0789150
Kevin Chu <14960080+imkevinchu@users.noreply.github.com>
2023-01-27 12:34:26 -0800

    import astro blog template

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,9 +1,2 @@
+/// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
-
-interface ImportMetaEnv {
-	readonly PUBLIC_VERCEL_ANALYTICS_ID: string;
-}
-
-interface ImportMeta {
-	readonly env: ImportMetaEnv;
-}
