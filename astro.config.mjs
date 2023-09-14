75bfd7bdbf7a61d06c80c58108ae29f78ab58c80
Kevin Chu <14960080+imkevinchu@users.noreply.github.com>
2023-09-14 14:09:29 -0700

    upgrade astro to 3.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,9 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://soupbonecollective.org",
-  integrations: [
-    mdx(),
-    sitemap(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-  ],
+  integrations: [mdx(), sitemap()],
 });
