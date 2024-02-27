2dd83f9d5e000041d91e31148feebd110c2dbaac
Kevin Chu <14960080+imkevinchu@users.noreply.github.com>
2024-02-27 10:57:33 -0800

    enable vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://soupbonecollective.org",
   integrations: [mdx(), sitemap()],
+  output: "server",
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
 });
