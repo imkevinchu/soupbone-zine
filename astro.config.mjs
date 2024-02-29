35f0b7d7bbea65f7fa0482120dac898817e2236c
Kevin Chu <14960080+imkevinchu@users.noreply.github.com>
2024-02-28 20:07:42 -0800

    Revert "enable vercel analytics"
    This reverts commit 2dd83f9d5e000041d91e31148feebd110c2dbaac.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,9 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://soupbonecollective.org",
   integrations: [mdx(), sitemap()],
-  output: "server",
-  adapter: vercel({
-    webAnalytics: { enabled: true },
-  }),
 });
